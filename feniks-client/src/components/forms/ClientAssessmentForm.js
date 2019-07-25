c693ec7ab2ca9bafbf04bf790608a2a938ec7131
James Budge <thebudgie@gmail.com>
2019-07-25 14:57:36 +0100

    gave all the checkboxes and their labels names. descriptive ones.

--- feniks-client/src/components/forms/ClientAssessmentForm.js
+++ feniks-client/src/components/forms/ClientAssessmentForm.js
@@ -266,101 +266,101 @@ class ClientAssessmentForm extends Component {
               <h4>Presenting Issues</h4>
             </div>
             <div className="form-group form-check form-check-inline">
-              <input type="checkbox" id="1" name="1" className="form-check-input" />
-              <label className="form-check-label" htmlFor="1">Abuse / Trauma</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Adaptation Problems</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Addiction: Alcohol</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Addiction: Drugs</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Addiction: Gambling</label>
+              <input type="checkbox" id="abuseTraumaCheckbox" name="abuseTraumaCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="abuseTraumaCheckbox">Abuse / Trauma</label>
+              <input type="checkbox" id="adaptationCheckbox" name="adaptationCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="adaptationCheckbox">Adaptation Problems</label>
+              <input type="checkbox" id="alcoholCheckbox" name="alcoholCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="alcoholCheckbox">Addiction: Alcohol</label>
+              <input type="checkbox" id="drugsCheckbox" name="drugsCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="drugsCheckbox">Addiction: Drugs</label>
+              <input type="checkbox" id="gamblingCheckbox" name="gamblingCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="gamblingCheckbox">Addiction: Gambling</label>
             </div>
             <br />
             <div className="form-group form-check form-check-inline">
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Addiction: Other</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Alcohol / substance abuse in family of origin</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Anger and Aggression</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Anxiety</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Bereavement/Loss</label>
+              <input type="checkbox" id="otherAddictionCheckbox" name="otherAddictionCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="otherAddictionCheckbox">Addiction: Other</label>
+              <input type="checkbox" id="substanceAbuseHistoryCheckbox" name="substanceAbuseHistoryCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="substanceAbuseHistoryCheckbox">Alcohol / substance abuse in family of origin</label>
+              <input type="checkbox" id="angerAgressionCheckbox" name="angerAgressionCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="angerAgressionCheckbox">Anger and Aggression</label>
+              <input type="checkbox" id="anxietyCheckbox" name="anxietyCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="anxietyCheckbox">Anxiety</label>
+              <input type="checkbox" id="bereavementCheckbox" name="bereavementCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="bereavementCheckbox">Bereavement/Loss</label>
             </div>
             <br />
             <div className="form-group form-check form-check-inline">
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Bullying / Mobbing</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Childhood Issues</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Cognitive / Learning</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Dealing with Emotions</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Depression</label>
+              <input type="checkbox" id="bullyingCheckbox" name="bullyingCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="bullyingCheckbox">Bullying / Mobbing</label>
+              <input type="checkbox" id="childhoodIssuesCheckbox" name="childhoodIssuesCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="childhoodIssuesCheckbox">Childhood Issues</label>
+              <input type="checkbox" id="cognitiveLearningCheckbox" name="cognitiveLearningCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="cognitiveLearningCheckbox">Cognitive / Learning</label>
+              <input type="checkbox" id="emotionsCheckbox" name="emotionsCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="emotionsCheckbox">Dealing with Emotions</label>
+              <input type="checkbox" id="depressionCheckbox" name="depressionCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="depressionCheckbox">Depression</label>
             </div>
             <br />
             <div className="form-group form-check form-check-inline">
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Domestic Violence</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Eating Disorder</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Family Difficulties</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Gender / Sexuality</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Interpersonal / Relationships</label>
+              <input type="checkbox" id="domesticViolenceCheckbox" name="domesticViolenceCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="domesticViolenceCheckbox">Domestic Violence</label>
+              <input type="checkbox" id="eatingDisorderCheckbox" name="eatingDisorderCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="eatingDisorderCheckbox">Eating Disorder</label>
+              <input type="checkbox" id="familyDifficultyCheckbox" name="familyDifficultyCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="familyDifficultyCheckbox">Family Difficulties</label>
+              <input type="checkbox" id="genderSexualityCheckbox" name="genderSexualityCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="genderSexualityCheckbox">Gender / Sexuality</label>
+              <input type="checkbox" id="interpersonalRelationshipsCheckbox" name="interpersonalRelationshipsCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="interpersonalRelationshipsCheckbox">Interpersonal / Relationships</label>
             </div>
             <br />
             <div className="form-group form-check form-check-inline">
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Isolation</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Living / Welfare</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Mental Health Diagnosis</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Mood Disorders</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Panic Attacks</label>
+              <input type="checkbox" id="isolationCheckbox" name="isolationCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="isolationCheckbox">Isolation</label>
+              <input type="checkbox" id="livingWelfareCheckbox" name="livingWelfareCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="livingWelfareCheckbox">Living / Welfare</label>
+              <input type="checkbox" id="mentalHealthCheckbox" name="mentalHealthCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="mentalHealthCheckbox">Mental Health Diagnosis</label>
+              <input type="checkbox" id="moodDisorderCheckbox" name="moodDisorderCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="moodDisorderCheckbox">Mood Disorders</label>
+              <input type="checkbox" id="panicAttackCheckbox" name="panicAttackCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="panicAttackCheckbox">Panic Attacks</label>
             </div>
             <br />
             <div className="form-group form-check form-check-inline">
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Personality Disorders</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Phobias</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Psychosis</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Self-Esteem</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Self-Harming</label>
+              <input type="checkbox" id="personalityDisorderCheckbox" name="personalityDisorderCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="personalityDisorderCheckbox">Personality Disorders</label>
+              <input type="checkbox" id="phobiasCheckBox" name="phobiasCheckBox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="phobiasCheckBox">Phobias</label>
+              <input type="checkbox" id="psychosisCheckbox" name="psychosisCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="psychosisCheckbox">Psychosis</label>
+              <input type="checkbox" id="selfEsteemCheckbox" name="selfEsteemCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="selfEsteemCheckbox">Self-Esteem</label>
+              <input type="checkbox" id="selfHarmCheckbox" name="selfHarmCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="selfHarmCheckbox">Self-Harming</label>
             </div>
             <br />
             <div className="form-group form-check form-check-inline">
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Sexual</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Stress</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Studies</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Suicidal Thoughts / Feelings</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Suicidal Attempts</label>
+              <input type="checkbox" id="sexualCheckbox" name="sexualCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="sexualCheckbox">Sexual</label>
+              <input type="checkbox" id="stressCheckbox" name="stressCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="stressCheckbox">Stress</label>
+              <input type="checkbox" id="studiesCheckbox" name="studiesCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="studiesCheckbox">Studies</label>
+              <input type="checkbox" id="suicidalThoughtsCheckbox" name="suicidalThoughtsCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="suicidalThoughtsCheckbox">Suicidal Thoughts / Feelings</label>
+              <input type="checkbox" id="suicidalAttemptsCheckbox" name="suicidalAttemptsCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="suicidalAttemptsCheckbox">Suicidal Attempts</label>
             </div>
             <br />
             <div className="form-group form-check form-check-inline">
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Work</label>
-              <input type="checkbox" id="" name="" className="form-check-input" />
-              <label className="form-check-label" htmlFor="">Other</label>
+              <input type="checkbox" id="workCheckbox" name="workCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="workCheckbox">Work</label>
+              <input type="checkbox" id="otherCheckbox" name="otherCheckbox" className="form-check-input" />
+              <label className="form-check-label" htmlFor="otherCheckbox">Other</label>
             </div>
 
             <div className="form-group">
