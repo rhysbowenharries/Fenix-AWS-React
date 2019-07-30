37e781d5bc4f06dcf564120d1831f303718746ef
Diego Diniz da Silva <diegodinizz@gmail.com>
2019-07-30 15:06:49 +0100

    logo fixed

--- feniks-client/src/containers/Home.js
+++ feniks-client/src/containers/Home.js
@@ -3,14 +3,15 @@ import DateSearch from './DateSearch.js'
 import castle from '../images/home-bg.jpg'
 import AppointmentList from './AppointmentList.js'
 import  './Home.css'
-import Login from './loginContainers/Login'
+import logo from "../images/feniks_logo-1.jpg"
 
 
 // this should be a class if we are going to have a dash board.
 const Home = () =>(
-  <div>
-      <h2>Login Page</h2>
-      <Login />
+  <div className="home">
+    <a href="/">
+    <img src={logo} alt="feniks-logo" className="feniks-logo" />
+    </a>
   </div>
 )
 
