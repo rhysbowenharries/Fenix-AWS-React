bdcf260d3b3c68cb93beb08f2011645a68bd6dc8
Ben Hamilton <ben.hamilton@me.com>
2019-07-31 12:23:27 +0100

    moved new api to top level and added CRUD routes ready to deploy

---
+++ feniks-app-api/create.js
@@ -0,0 +1,23 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "clients",
+    Item: {
+      id: uuid.v1(),
+      address: data.address,
+      projects:data.project,
+      client: data.client
+    }
+  };
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
